9db80e9b78f744460cf36418a10ece1dcef85b3c
租小二 <zux2@zux2.com>
2018-11-10 17:52:18 +0800

    1

--- js/base.js
+++ js/base.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 		if (deviceWidth > deviceHeight) {
 			/*alert('亲 竖屏才是正确浏览方式');*/
 		}
-		document.documentElement.style.fontSize = minLenth / 7.5 + 'px'; alert(deviceWidth)
+                  document.documentElement.style.fontSize = minLenth / 27.5 + 'px'; alert(deviceWidth);
 	}
 	resizeFontSize();
 })
